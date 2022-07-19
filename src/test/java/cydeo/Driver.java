29a6e53b878e4b74269830ebbe6aa339c21fc320
igo-or <bart1-1@mail.ru>
2022-07-18 23:31:37 -0500

    Updated ip

--- src/test/java/cydeo/Driver.java
+++ src/test/java/cydeo/Driver.java
@@ -35,7 +35,7 @@ public class Driver {
                 case "remote-chrome":
                     try {
                         // assign your grid server address
-                        String gridAddress = "34.236.155.245";
+                        String gridAddress = "34.224.82.204";
                         URL url = new URL("http://" + gridAddress + ":4444/wd/hub");
                         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
                         desiredCapabilities.setBrowserName("chrome");
@@ -51,7 +51,7 @@ public class Driver {
 
                     try {
                         // assign your grid server address
-                        String gridAddress = "34.236.155.245";
+                        String gridAddress = "34.224.82.204";
                         URL url = new URL("http://" + gridAddress + ":4444/wd/hub");
                         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
                         desiredCapabilities.setBrowserName("firefox");
